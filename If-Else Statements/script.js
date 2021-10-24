f053716cfdf1b7bc05b855bb28129304dd9cad7e
Bierloga <90850242+Bierloga@users.noreply.github.com>
2021-10-24 20:40:57 +0200

    Add files via upload

---
+++ If-Else Statements/script.js
@@ -0,0 +1,17 @@
+const age = 19
+const isFemale = true
+const driverStatus = "bob"
+
+if (age <= 18) {
+    console.log("U mag niet naar binnen!")
+} else if (isFemale == false) {
+    console.log("U mag niet naar binnen.")
+} else {
+    console.log("U mag naar binnen!")
+}
+
+if (driverStatus == "bob") {
+    console.log("u mag rijden!")
+} else {
+    console.log("U mag niet rijden")
+}
